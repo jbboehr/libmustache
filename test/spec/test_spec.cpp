becfe72b24aba1b26b219779f9b027042a085ff3
John Boehr <jbboehr@gmail.com>
2013-06-02 23:07:01 -0700

    working on the specification tests

--- test/spec/test_spec.cpp
+++ test/spec/test_spec.cpp
@@ -1,5 +1,84 @@
 
+#include "test_spec.hpp"
+
 int main( int argc, char * argv[] )
 {
-  return 1;
+  if( argc < 2 ) {
+    fprintf(stderr, "Requires at least one argument\n");
+    return 1;
+  }
+  
+  DIR * dir;
+  struct dirent * ent;
+  char * directory = argv[1];
+  if( (dir = opendir(directory)) == NULL ) {
+    fprintf(stderr, "Unable to open directory\n");
+    return 1;
+  }
+  
+  int nFiles = 0;
+  while( (ent = readdir(dir)) != NULL ) {
+    if( ent->d_name[0] == '.' ) continue;
+    if( strlen(ent->d_name) < 5 ) continue;
+    if( strcmp(ent->d_name + strlen(ent->d_name) - 4, ".yml") != 0 ) continue;
+    if( nFiles >= MAX_TEST_FILES ) continue;
+    
+    // Make filename
+    char * file = ent->d_name;
+    std::string fileName;
+    fileName += directory;
+    fileName += file;
+    
+    std::ifstream pFile(fileName.c_str());
+    if( !pFile.is_open() ) {
+      continue;
+    }
+    
+    // get length of file:
+    pFile.seekg (0, pFile.end);
+    int length = pFile.tellg();
+    pFile.seekg (0, pFile.beg);
+    
+    // read file data
+    char * fileData = new char[length];
+    pFile.read(fileData, length);
+    pFile.close();
+    
+    // parse the file
+    parse_file(fileData, length);
+  }
+  closedir(dir);
+  
+  
+  return 0;
+}
+
+void parse_file(char * fileData, int length)
+{
+  // start yaml parser
+  yaml_parser_t parser;
+  yaml_event_t event;
+  yaml_parser_initialize(&parser);
+
+  const unsigned char * input = reinterpret_cast<const unsigned char *>(fileData);
+
+  yaml_parser_set_input_string(&parser, input, length);
+
+  int done = 0;
+  while (!done) {
+    if( !yaml_parser_parse(&parser, &event) ) {
+      return;
+    }
+
+    switch( event.type ) {
+
+    }
+
+    /* Are we finished? */
+    done = (event.type == YAML_STREAM_END_EVENT);
+
+    /* The application is responsible for destroying the event object. */
+    yaml_event_delete(&event);
+  }
+  yaml_parser_delete(&parser);
 }
--- test/spec/test_spec.hpp
+++ test/spec/test_spec.hpp
@@ -3,6 +3,18 @@
 #include <config.h>
 #endif
 
+#define MAX_TEST_FILES 50
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <yaml.h>
+#include <sys/stat.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+void parse_file(char * fileData, int length);
