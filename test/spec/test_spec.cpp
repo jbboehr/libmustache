883a0a92808a9585891b2ea7f1fb6f4c309c69c4
John Boehr <jbboehr@gmail.com>
2013-06-03 20:12:45 -0700

    Working on specification tests

--- test/spec/test_spec.cpp
+++ test/spec/test_spec.cpp
@@ -1,6 +1,8 @@
 
 #include "test_spec.hpp"
 
+std::list<MustacheSpecTest *> tests; 
+
 int main( int argc, char * argv[] )
 {
   if( argc < 2 ) {
@@ -21,12 +23,14 @@ int main( int argc, char * argv[] )
     if( ent->d_name[0] == '.' ) continue;
     if( strlen(ent->d_name) < 5 ) continue;
     if( strcmp(ent->d_name + strlen(ent->d_name) - 4, ".yml") != 0 ) continue;
+    if( *(ent->d_name) == '~' ) continue; // Ignore lambdas
     if( nFiles >= MAX_TEST_FILES ) continue;
     
     // Make filename
     char * file = ent->d_name;
     std::string fileName;
     fileName += directory;
+    fileName += '/';
     fileName += file;
     
     std::ifstream pFile(fileName.c_str());
@@ -45,40 +49,155 @@ int main( int argc, char * argv[] )
     pFile.close();
     
     // parse the file
+    std::cout << fileName << "\n";
     parse_file(fileData, length);
   }
   closedir(dir);
   
   
-  return 0;
+  // Summarize
+  std::list<MustacheSpecTest *>::iterator it = tests.begin();
+  int nPassed = 0;
+  int nFailed = 0;
+  for( ; it != tests.end(); ++it ) {
+    if( (*it)->passed() ) {
+      nPassed++;
+    } else {
+      nFailed++;
+    }
+  }
+  int total = nPassed + nFailed;
+  printf("%d passed, %d failed of %d tests\n", nPassed, nFailed, total);
+  return (nFailed > 0 ? 1 : 0);
 }
 
 void parse_file(char * fileData, int length)
 {
   // start yaml parser
   yaml_parser_t parser;
+  yaml_document_t document;
   yaml_event_t event;
   yaml_parser_initialize(&parser);
 
   const unsigned char * input = reinterpret_cast<const unsigned char *>(fileData);
 
   yaml_parser_set_input_string(&parser, input, length);
+  yaml_parser_load(&parser, &document);
+  mustache_spec_parse_document(&document);
+  yaml_document_delete(&document);
+}
 
-  int done = 0;
-  while (!done) {
-    if( !yaml_parser_parse(&parser, &event) ) {
-      return;
-    }
 
-    switch( event.type ) {
-
-    }
-
-    /* Are we finished? */
-    done = (event.type == YAML_STREAM_END_EVENT);
-
-    /* The application is responsible for destroying the event object. */
-    yaml_event_delete(&event);
+void mustache_spec_parse_document(yaml_document_t * document)
+{
+  yaml_node_t * node = yaml_document_get_root_node(document);
+  if( node->type != YAML_MAPPING_NODE ) {
+    return;
   }
-  yaml_parser_delete(&parser);
+  
+  yaml_node_pair_t * pair;
+  for( pair = node->data.mapping.pairs.start; pair < node->data.mapping.pairs.top; pair++ ) {
+    yaml_node_t * keyNode = yaml_document_get_node(document, pair->key);
+    yaml_node_t * valueNode = yaml_document_get_node(document, pair->value);
+    char * keyValue = reinterpret_cast<char *>(keyNode->data.scalar.value);
+    if( strcmp(keyValue, "tests") == 0 &&
+        valueNode->type == YAML_SEQUENCE_NODE ) {
+      mustache_spec_parse_tests(document, valueNode);
+    }
+  }
+}
+
+void mustache_spec_parse_tests(yaml_document_t * document, yaml_node_t * node)
+{
+  if( node->type != YAML_SEQUENCE_NODE ) {
+    return;
+  }
+  
+  yaml_node_item_t *item;
+  for( item = node->data.sequence.items.start; item < node->data.sequence.items.top; item ++) {
+    yaml_node_t * valueNode = yaml_document_get_node(document, *item);
+    if( valueNode->type == YAML_MAPPING_NODE ) {
+      mustache_spec_parse_test(document, valueNode);
+    }
+  }
+}
+
+void mustache_spec_parse_test(yaml_document_t * document, yaml_node_t * node)
+{
+  if( node->type != YAML_MAPPING_NODE ) {
+    return;
+  }
+  
+  MustacheSpecTest * test = new MustacheSpecTest;
+  
+  yaml_node_pair_t * pair;
+  for( pair = node->data.mapping.pairs.start; pair < node->data.mapping.pairs.top; pair++ ) {
+    yaml_node_t * keyNode = yaml_document_get_node(document, pair->key);
+    yaml_node_t * valueNode = yaml_document_get_node(document, pair->value);
+    char * keyValue = reinterpret_cast<char *>(keyNode->data.scalar.value);
+    
+    if( valueNode->type == YAML_SCALAR_NODE ) {
+      char * valueValue = reinterpret_cast<char *>(valueNode->data.scalar.value);
+      if( strcmp(keyValue, "name") == 0 ) {
+        test->name.assign(valueValue);
+      } else if( strcmp(keyValue, "desc") == 0 ) {
+        test->desc.assign(valueValue);
+      } else if( strcmp(keyValue, "template") == 0 ) {
+        test->tmpl.assign(valueValue);
+      } else if( strcmp(keyValue, "expected") == 0 ) {
+        test->expected.assign(valueValue);
+      }
+    } else if( valueNode->type == YAML_MAPPING_NODE ) {
+      if( strcmp(keyValue, "data") == 0 ) {
+        mustache_spec_parse_data(document, valueNode, &test->data);
+      } else if( strcmp(keyValue, "partials") == 0 ) {
+        mustache_spec_parse_partials(document, valueNode, &test->partials);
+      }
+    }
+  }
+  
+  // Execute the test
+  mustache::Mustache mustache;
+  mustache::Node root;
+  mustache.tokenize(&test->tmpl, &root);
+  mustache.render(&root, &test->data, &test->partials, &test->output);
+  
+  // Output result
+  test->print();
+  tests.push_back(test);
+}
+
+void mustache_spec_parse_data(yaml_document_t * document, yaml_node_t * node, mustache::Data * data)
+{
+  if( node->type == YAML_MAPPING_NODE ) {
+    yaml_node_pair_t * pair;
+    
+    data->init(mustache::Data::TypeMap, 0);
+    
+    for( pair = node->data.mapping.pairs.start; pair < node->data.mapping.pairs.top; pair++ ) {
+      yaml_node_t * keyNode = yaml_document_get_node(document, pair->key);
+      yaml_node_t * valueNode = yaml_document_get_node(document, pair->value);
+      char * keyValue = reinterpret_cast<char *>(keyNode->data.scalar.value);
+      mustache::Data * child = new mustache::Data();
+      mustache_spec_parse_data(document, valueNode, child);
+      data->data.insert(std::pair<std::string,mustache::Data*>(keyValue,child));
+    }
+  } else if( node->type == YAML_SEQUENCE_NODE ) {
+    yaml_node_item_t * item;
+    int nItems = node->data.sequence.items.top - node->data.sequence.items.start;
+    data->init(mustache::Data::TypeArray, nItems);
+    for( item = node->data.sequence.items.start; item < node->data.sequence.items.top; item ++) {
+      yaml_node_t * valueNode = yaml_document_get_node(document, *item);
+      mustache_spec_parse_data(document, valueNode, &data->array[item - node->data.sequence.items.start]);
+    }
+  } else if( node->type == YAML_SCALAR_NODE ) {
+    char * keyValue = reinterpret_cast<char *>(node->data.scalar.value);
+    data->init(mustache::Data::TypeString, node->data.scalar.length);
+    data->val->assign(keyValue);
+  }
+}
+
+void mustache_spec_parse_partials(yaml_document_t * document, yaml_node_t * node, mustache::Node::Partials * partials)
+{
+  // @todo
 }
--- test/spec/test_spec.hpp
+++ test/spec/test_spec.hpp
@@ -16,5 +16,51 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <exception>
+
+#include "mustache.hpp";
+
+class MustacheSpecTest {
+  private:
+  public:
+    std::string name;
+    std::string desc;
+    mustache::Data data;
+    std::string tmpl;
+    mustache::Node::Partials partials;
+    std::string expected;
+    std::string output;
+    bool passed() {
+      return (expected == output);
+    };
+    void print() {
+      
+      std::cout << name << " ... " 
+                << (passed() ? "PASSED" : "FAILED")
+                << "\n";
+              
+    }
+    std::string toString() {
+      std::string ret;
+      ret.append("name=");
+      ret.append(name);
+      ret.append(", ");
+      ret.append("desc=");
+      ret.append(desc);
+      ret.append(", ");
+      ret.append("tmpl=");
+      ret.append(tmpl);
+      ret.append(", ");
+      ret.append("expected=");
+      ret.append(expected);
+      return ret;
+    }
+};
 
 void parse_file(char * fileData, int length);
+
+void mustache_spec_parse_document(yaml_document_t * document);
+void mustache_spec_parse_tests(yaml_document_t * document, yaml_node_t * node);
+void mustache_spec_parse_test(yaml_document_t * document, yaml_node_t * node);
+void mustache_spec_parse_data(yaml_document_t * document, yaml_node_t * node, mustache::Data * data);
+void mustache_spec_parse_partials(yaml_document_t * document, yaml_node_t * node, mustache::Node::Partials * partials);
