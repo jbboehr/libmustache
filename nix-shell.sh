0af0df39edf97abe0606f95da1c7db95d551b252
John Boehr <john@johnboehr.is>
2018-02-06 13:13:55 -0800

    Add pkgconfig to nix-shell.sh

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec nix-shell -p autoconf -p gcc -p m4 -p libtool -p automake -p pcre -p libyaml
+exec nix-shell -p autoconf -p gcc -p m4 -p libtool -p automake -p pcre -p libyaml -p pkgconfig
