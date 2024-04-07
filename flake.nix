4f003a4620994e8817a8d8088acbae3ffd072726
John Boehr <john@johnboehr.is>
2024-04-07 11:17:18 -0700

    Update to nixos 23.11

--- flake.nix
+++ flake.nix
@@ -2,13 +2,25 @@
   description = "libmustache";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    mustache_spec.url = "github:jbboehr/mustache-spec";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mustache_spec.url = "github:jbboehr/mustache-spec";
   };
 
   outputs = {
@@ -17,10 +29,46 @@
     flake-utils,
     mustache_spec,
     gitignore,
+    pre-commit-hooks,
+    systems,
+    nix-github-actions,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+        src' = gitignore.lib.gitignoreSource ./.;
+
+        src = pkgs.lib.cleanSourceWith {
+          name = "libmustache-source";
+          src = src';
+          filter = gitignore.lib.gitignoreFilterWith {
+            basePath = ./.;
+            extraRules = ''
+              .clang-format
+              composer.json
+              composer.lock
+              .editorconfig
+              .envrc
+              .gitattributes
+              .github
+              .gitignore
+              *.md
+              *.nix
+              flake.*
+            '';
+          };
+        };
+
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = src';
+          hooks = {
+            actionlint.enable = true;
+            alejandra.enable = true;
+            shellcheck.enable = true;
+          };
+        };
       in rec {
         packages = flake-utils.lib.flattenTree rec {
           libmustache = pkgs.callPackage ./default.nix {
@@ -38,7 +86,27 @@
 
         checks = packages;
 
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [packages.default];
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+          '';
+        };
+
         formatter = pkgs.alejandra;
       }
-    );
+    )
+    // {
+      # prolly gonna break at some point
+      githubActions.matrix.include = let
+        cleanFn = v: v // {name = builtins.replaceStrings ["githubActions." "checks." "x86_64-linux."] ["" "" ""] v.attr;};
+      in
+        builtins.map cleanFn
+        (nix-github-actions.lib.mkGithubMatrix {
+          attrPrefix = "checks";
+          checks = nixpkgs.lib.getAttrs ["x86_64-linux"] self.checks;
+        })
+        .matrix
+        .include;
+    };
 }
